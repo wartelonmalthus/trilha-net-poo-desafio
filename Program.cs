7f2498fc0caa60bdafb403f99b95efa457da35e9
Wartelon Malthus <wartelon@implantatecnologia.com.br>
2024-11-04 11:18:35 -0300

    Resolvendo o projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarthphone Nokia: ");
+Smartphone Nokia = new Nokia(numero: "13154", modelo: "Modelo 01", imei: "11111111", memoria: 64);
+
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n"):
+
+Console.WriteLine("Smarthphone Iphone");
+Smartphone iphone = new Iphone(numero: "46854", modelo: "modelo 02", imei: "11111111", memoria: 124);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
